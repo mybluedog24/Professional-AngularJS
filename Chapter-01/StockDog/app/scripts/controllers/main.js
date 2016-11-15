9e2f57d7673a7d71ea75028cc87b2e492834fb20
mybluedog24 <frankchen4096@gmail.com>
2016-11-15 04:05:38 -0500

    Chapter-01-Step-04: Done (TypeError:_.contains is not a function)

--- Chapter-01/StockDog/app/scripts/controllers/main.js
+++ Chapter-01/StockDog/app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the stockDogApp
  */
 angular.module('stockDogApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $location, WatchlistService) {
+    // [1] Populate watchlists for dynamic nav links
+    $scope.watchlists = WatchlistService.query();
+
+    // [2] Using the $location.path() function as a $watch expression
+    $scope.$watch(function () {
+      return $location.path();
+    }, function (path) {
+      if (_.contains(path, 'watchlist')) {
+        $scope.activeView = 'watchlist';
+      } else {
+        $scope.activeView = 'dashboard';
+      }
+    });
   });
