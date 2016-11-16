61a20a18f6b32628f09a6479e5fcbdb17df1befa
mybluedog24 <frankchen4096@gmail.com>
2016-11-15 20:24:30 -0500

    Chapter-01-Step-05: Done

--- Chapter-01/StockDog/app/scripts/services/watchlist-service.js
+++ Chapter-01/StockDog/app/scripts/services/watchlist-service.js
@@ -11,6 +11,49 @@ angular.module('stockDogApp')
   .service('WatchlistService', function WatchlistService() {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
+    // Augment Stocks with additional helper functions
+    var StockModel = {
+      save: function () {
+        var watchlist = findById(this.listId);
+        watchlist.recalculate();
+        saveModel();
+      }
+    };
+
+    // Augment watchlists with additional helper functions
+    var WatchlistModel = {
+      addStock: function (stock) {
+        var existingStock = _.find(this.stocks, function (s) {
+          return s.company.symbol === stock.company.symbol;
+        });
+        if (existingStock) {
+          existingStock.shares += stock.shares;
+          this.stocks.push(stock);
+        }
+        this.recalculate();
+        saveModel();
+      },
+      removeStock: function (stock) {
+        _.remove(this.stocks, function (s) {
+          return s.company.symbol === stock.company.symbol;
+        });
+        this.recalculate();
+        saveModel();
+      },
+      recalculate: function () {
+        var calcs = _.reduce(this.stocks, function (calcs, stock) {
+          calcs.shares += stock.shares;
+          calcs.marketValue += stock.marketValue;
+          calcs.dayChange += stock.dayChange;
+          return calcs;
+        }, { shares: 0, marketValue: 0, dayChange: 0 });
+
+        this.shares = calcs.shares;
+        this.marketValue = calcs.marketValue;
+        this.dayChange = calcs.dayChange;
+      }
+    };
+
     // [1] Helper: Load watchlists from localStorage
     var loadModel = function () {
       var model = {
@@ -19,6 +62,12 @@ angular.module('stockDogApp')
         nextId: localStorage['StockDog.nextId'] ?
           parseInt(localStorage['StockDog.nextId']) : 0
       };
+      _.each(model.watchlists, function (watchlist){
+        _.extend(watchlist, WatchlistModel);
+        _.each(watchlist.stocks, function (stock) {
+          _.extend(stock, StockModel);
+        });
+      });
       return model;
     };
 
@@ -47,6 +96,8 @@ angular.module('stockDogApp')
     // [5] Save a new watchlist to watchlists model
     this.save = function (watchlist) {
       watchlist.id = Model.nextId++;
+      watchlist.stocks = [];
+      _.extend(watchlist, WatchlistModel);
       Model.watchlists.push(watchlist);
       saveModel();
     };
